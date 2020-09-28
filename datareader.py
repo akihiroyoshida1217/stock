9be77c1f0bd129239c4722a3df920ad7d273e8bb
akihiroyoshida1217 <akihiro.takahashi.1217@gmail.com>
2020-09-29 00:13:49 +0900

    change to multiprocessing

--- datareader.py
+++ datareader.py
@@ -1,47 +1,75 @@
 import pandas_datareader.data as web
 from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
-from tqdm import tqdm
+#from tqdm import tqdm
 import pandas as pd
 import codecs
+from multiprocessing import Pool
+import multiprocessing
 
-data = {}
-index_filtered = pd.DataFrame( columns = ['Volume', 'Close', 'Change-1', 'Change-1(%)', 'Change-5', 'Change-5(%)', 'Deviation'])
-error_symbols = []
-
-symbols = get_nasdaq_symbols()
-
-for s in tqdm(symbols.index):
+def read_data(symbol):
+    #data = {}
+    error_symbols = []
+    
     try:
-        data[s] = web.DataReader(s,"yahoo","2020/7/1")
-        data[s].to_csv("./data/" + s + ".csv")
-        if data[s]['Close'][-1] > data[s]['Close'][-6] > data[s]['Close'][-21] and data[s]['Volume'][-1] > 1000000 and ((data[s]['Close'] - data[s]['Close'].rolling(window=25).mean()) / data[s]['Close'].rolling(window=25).mean() * 100)[-1] >= 1:
-            index_filtered.loc[s] = [ data[s]['Volume'][-1], data[s]['Close'][-1], data[s]['Close'][-1] - data[s]['Close'][-2], (data[s]['Close'][-1] - data[s]['Close'][-2]) / data[s]['Close'][-2] * 100, data[s]['Close'][-1] - data[s]['Close'][-6], (data[s]['Close'][-1] - data[s]['Close'][-6]) / data[s]['Close'][-6] * 100, ((data[s]['Close'] - data[s]['Close'].rolling(window=25).mean()) / data[s]['Close'].rolling(window=25).mean() * 100)[-1] ]
+        data = web.DataReader(symbol,"yahoo","2020/7/1")
+        data.to_csv("./data/" + symbol + ".csv")
+        return data
     except:
-        error_symbols.append(s)
-print(data, file=codecs.open('data.txt', 'w', 'utf-8'))
+        error_symbols.append(symbol)
 
+def search_data(data):
+    
+    try:
+        deviation = (data['Close'] - data['Close'].rolling(window=25).mean()) / data['Close'].rolling(window=25).mean() * 100
+        return data['Volume'][-1] > 1000000 \
+            and data['Close'][-1] > data['Close'][-6] > data['Close'][-21] \
+            and deviation[-1] >= 3 \
+            and (deviation[-21:-1] >= -1).sum() == 20
+    except:
+        return False
 
-#data = [ web.DataReader(s,"yahoo","2020/7/1") for s in tqdm(symbols.index) ]
+def get_data(item):
+    error_symbols = []
+    symbol = item[0] 
+    data = item[1] 
+    
+    try:
+        deviation = (data['Close'] - data['Close'].rolling(window=25).mean()) / data['Close'].rolling(window=25).mean() * 100
+        data_extracted = [ symbol,
+            data['Volume'][-1], 
+            data['Close'][-1], data['Close'][-1] - data['Close'][-2], 
+            (data['Close'][-1] - data['Close'][-2]) / data['Close'][-2] * 100, 
+            data['Close'][-1] - data['Close'][-6], 
+            (data['Close'][-1] - data['Close'][-6]) / data['Close'][-6] * 100, 
+            deviation[-1] ]
+        return data_extracted
+    except:
+        error_symbols.append(symbol)
 
-#data["GOOGL"] = web.DataReader("GOOGL","yahoo","2020/7/1")
-#data["FAB"] = web.DataReader("FAB","yahoo","2020/7/1")
-##print(data)
-#print(data, file=codecs.open('test2.txt', 'w', 'utf-8'))
+if __name__ == '__main__':
 
-#print(((data["GOOGL"]['Close'] - data["GOOGL"]['Close'].rolling(window=25).mean()) / data["GOOGL"]['Close'].rolling(window=25).mean() * 100)[-1])
-#index_filtered = [ i for i, d in data.items() if d['Close'][-1] > d['Close'][-6] > d['Close'][-21]
-#                                                and ((d['Close'] - d['Close'].rolling(window=25).mean()) / d['Close'].rolling(window=25).mean() * 100)[-1] >= 1 ]
-#[ data[fi].to_csv(fi + ".csv") for fi in index_filtered ]
+    symbols = get_nasdaq_symbols()
 
-index_filtered_sorted = index_filtered.sort_values('Close')
-index_filtered_sorted.to_csv("index.csv")
-print(index_filtered_sorted)
+    with Pool(multiprocessing.cpu_count()) as pool_for_read:
+        index_dataframe = {i: d for i, d in zip(symbols.index, pool_for_read.map(read_data, symbols.index))}
 
-#data = web.DataReader("GOOGL","yahoo","2020/7/1")
-#data.to_csv("sample.csv")
-#print(data)
-#
-#print(data['Close'][-1])
-#print(data['Close'][-6])
-#print(data['Close'][-21])
+    with Pool(multiprocessing.cpu_count()) as pool_for_search:
+        index_dataframe_filtered = {i: d for i, d, result in zip(list(index_dataframe.keys()), list(index_dataframe.values()), pool_for_search.map(search_data, list(index_dataframe.values()))) if result}
+
+    with Pool(multiprocessing.cpu_count()) as pool_for_get:
+        index_data_extracted = pool_for_get.map(get_data, list(index_dataframe_filtered.items()))
+
+    index_dataframe_extracted = pd.DataFrame( index_data_extracted, columns = ['symbol', 'Volume', 'Close', 'Change-1', 'Change-1(%)', 'Change-5', 'Change-5(%)', 'Deviation']).set_index('symbol')
+    index_dataframe_sorted = index_dataframe_extracted.sort_values('Close')
+    index_dataframe_sorted.to_csv("index.csv")
+    print(index_dataframe_sorted)
+
+    #index_filtered_list = list(map(symbol_search, symbols.index))
+    #print(index_filtered_list, file=codecs.open('index_filtered_list.txt', 'w', 'utf-8'))
+
+    #index_filtered = pd.DataFrame( index_filtered_list, columns = ['symbol', 'Volume', 'Close', 'Change-1', 'Change-1(%)', 'Change-5', 'Change-5(%)', 'Deviation']).set_index('symbol')
+
+    #index_filtered_sorted = index_filtered.sort_values('Close')
+    #index_filtered_sorted.to_csv("index.csv")
+    #print(index_filtered_sorted)
 
